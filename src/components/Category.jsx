4fb24e1f792e8147b6480a71b2397b3ff177d91d
patrick mugambi <patrick@kali.kali>
2022-10-23 13:36:47 +0300

    add space between in search border

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,6 +6,7 @@ import { NavLink } from "react-router-dom";
 const Category = () => {
   return (
     <>
+    <List></List>
     <div className="flex items-center -mx-4 space-x-2 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-800 dark:text-gray-100">
 	<a rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-2  dark:border-gray-700 dark:text-gray-400">
   <SLink to={"/cuisines/Italian"}>
